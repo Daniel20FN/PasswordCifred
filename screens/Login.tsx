73519bae2b9137282b3f7747303097b2a8d7a30d
vhmolino <vichercar2003@gmail.com>
2024-06-11 16:54:54 +0200

    tocar para quitar teclado

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Keyboard, Text, TouchableWithoutFeedback, View } from "react-native";
 import {
   Button,
   Center,
@@ -14,44 +14,50 @@ import {
 
 export default function Login({ navigation }) {
   return (
-    <View>
-      <VStack
-        width="100%"
-        height="100%"
-        justifyContent={"center"}
-        space={4}
-        paddingLeft={50}
-        paddingRight={50}
-      >
-        <Center shadow={3} marginBottom={100}>
-          <Image
-            source={require("../assets/LogoTransparente.png")}
-            alt="Logo Encrypt"
-            size="xl"
-          />
-        </Center>
-        <FormControl mb="5">
-          <Heading>Iniciar Sesion</Heading>
-          <Divider />
-          <FormControl.Label>Usuario</FormControl.Label>
-          <Input />
-          <FormControl.Label>Password</FormControl.Label>
-          <Input type="password" />
-          <Button
-            onPress={() => navigation.navigate("PasswordList")}
-            marginTop={3}
-            size="sm"
-            bgColor="#B49134"
-          >
-            Entrar
-          </Button>
-          <Center marginTop={2}>
-            <Link onPress={() => navigation.navigate("Register")}>
-              Añadir Cuenta
-            </Link>
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+      }}
+    >
+      <View>
+        <VStack
+          width="100%"
+          height="100%"
+          justifyContent={"center"}
+          space={4}
+          paddingLeft={50}
+          paddingRight={50}
+        >
+          <Center shadow={3} marginBottom={100}>
+            <Image
+              source={require("../assets/LogoTransparente.png")}
+              alt="Logo Encrypt"
+              size="xl"
+            />
           </Center>
-        </FormControl>
-      </VStack>
-    </View>
+          <FormControl mb="5">
+            <Heading>Iniciar Sesion</Heading>
+            <Divider />
+            <FormControl.Label>Usuario</FormControl.Label>
+            <Input />
+            <FormControl.Label>Password</FormControl.Label>
+            <Input type="password" />
+            <Button
+              onPress={() => navigation.navigate("PasswordList")}
+              marginTop={3}
+              size="sm"
+              bgColor="#B49134"
+            >
+              Entrar
+            </Button>
+            <Center marginTop={2}>
+              <Link onPress={() => navigation.navigate("Register")}>
+                Añadir Cuenta
+              </Link>
+            </Center>
+          </FormControl>
+        </VStack>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
