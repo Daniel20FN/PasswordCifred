2c84907bb985c22697c162720fdc02275a03ae87
Daniel20FN <daniel1011083376@gmail.com>
2024-06-11 16:48:14 +0200

    Login Design

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,34 +1,52 @@
 import React from "react";
 import { Text, View } from "react-native";
-import { Button } from "native-base";
+import {
+  Button,
+  Center,
+  VStack,
+  Image,
+  FormControl,
+  Input,
+  Heading,
+  Divider,
+  Link,
+} from "native-base";
+
 export default function Login({ navigation }) {
   return (
-    <View
-      style={{
-        flex: 1,
-      }}
-    >
-      <Text>Hola en el Logiiin</Text>
-      <View
-        style={{
-          position: "absolute",
-          //top: 800,
-          bottom: 30,
-          left: 0,
-          right: 0,
-          paddingHorizontal: 20,
-          alignItems: "center",
-        }}
+    <View>
+      <VStack
+        width="100%"
+        height="100%"
+        justifyContent={"center"}
+        space={4}
+        paddingLeft={50}
+        paddingRight={50}
       >
-        <Button
-          size="md"
-          variant="outline"
-          colorScheme="secondary"
-          onPress={() => navigation.navigate("PasswordList")}
-        >
-          SECONDARY
-        </Button>
-      </View>
+        <Center shadow={3} marginBottom={100}>
+          <Image
+            source={require("../assets/LogoTransparente.png")}
+            alt="Logo Encrypt"
+            size="xl"
+          />
+        </Center>
+        <FormControl mb="5">
+          <Heading>Iniciar Sesion</Heading>
+          <Divider />
+          <FormControl.Label>Usuario</FormControl.Label>
+          <Input />
+          <FormControl.Label>Password</FormControl.Label>
+          <Input type="password" />
+          <Button marginTop={3} size="sm" bgColor="#B49134">
+            Entrar
+          </Button>
+          <Center marginTop={2}>
+            <Link onPress={() => navigation.navigate("Register")}>
+              Añadir Cuenta
+            </Link>
+          </Center>
+        </FormControl>
+      </VStack>
     </View>
   );
 }
