48c0a79379b114934be12289c96ddb1bf1415fb3
Daniel20FN <daniel1011083376@gmail.com>
2024-06-11 14:40:23 +0200

    Correcciones

--- screens/PasswordList.tsx
+++ screens/PasswordList.tsx
@@ -3,28 +3,8 @@ import { Button, Text, View, ViewBase } from "react-native";
 
 export default function Login({ navigation }) {
   return (
-    <View
-      style={{
-        flex: 1,
-      }}
-    >
+    <View>
       <Text>Hola en el Password List</Text>
-      <View
-        style={{
-          position: "absolute",
-          //top: 800,
-          bottom: 30,
-          left: 0,
-          right: 0,
-          paddingHorizontal: 20,
-          alignItems: "center",
-        }}
-      >
-        <Button
-          title="Go to Login"
-          onPress={() => navigation.navigate("Login")}
-        />
-      </View>
     </View>
   );
 }
